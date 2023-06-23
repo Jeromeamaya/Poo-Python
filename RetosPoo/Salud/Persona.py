f9fa3584a7cddb32b8277f21257bc3cdd8fe0b61
Jerome <jaamaya748@misena.edu.co>
2023-06-23 12:26:04 -0500

    Encapsulamiento con get y set reto 3

--- RetosPoo/Salud/Persona.py
+++ RetosPoo/Salud/Persona.py
@@ -19,6 +19,35 @@ class Persona():
         self.estatura=estatura
         self.edad=edad
         self.sexo=sexo
+    
+    def getnombre(self):
+        return self.nombre    
+    def setNombre(self,nombre):
+        return self.nombre
+    def getApellido(self):
+        return self.apellido   
+    def setApellido(self):
+        return self.apellido
+    def getpeso(self):
+        return self.peso  
+    def setpeso(self):
+        return self.peso
+    def getEstatura(self):
+        return self.estatura 
+    def setEstatatura(self):
+        return self.estatura
+    def getEdad(self):
+        return self.edad
+    def setEdad(self):
+        return self.edad
+    def getSexo(self):
+        return self.sexo
+    def setSexo(self):
+        return self.sexo
+
+
+
+
 
     def pedirDatos(self):
         self.tipoDoc= input("Ingrese el tipo de documento \n ")
@@ -29,6 +58,8 @@ class Persona():
         self.estatura=float (input("Ingrese la estatura \n "))
         self.edad=int(input("Ingrese la edad \n "))
         self.sexo=input("Ingrese el tipo de sexo \n ")
+        
+        
         
     def mostrarPersonal (self):
         print(f"EL usuario {self.nombre} {self.apellido} con tipo de documento {self.tipoDoc} con numero {self.documento}")
